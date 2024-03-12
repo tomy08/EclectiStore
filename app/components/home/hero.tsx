11f780b27513a3c254841e989a0477ed9eed0f5c
tomy08 <tomasesantacruz@gmail.com>
2024-03-11 21:41:20 -0300

    fix ankor link

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -18,7 +18,7 @@ export function Hero() {
         <div className="mt-4 flex items-center justify-center gap-4">
           <Link
             className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-            href="/about"
+            href="/signup"
           >
             Sign In
           </Link>
